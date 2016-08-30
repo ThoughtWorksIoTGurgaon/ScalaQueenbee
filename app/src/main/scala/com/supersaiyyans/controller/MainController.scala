1e5930f34955b07961d525083a8d99d028427608
Dhruv Kapur <dhruvk@thoughtworks.com>
2016-08-30 18:33:00 +0530

    Dhruv| Formatting

--- app/src/main/scala/com/supersaiyyans/controller/MainController.scala
+++ app/src/main/scala/com/supersaiyyans/controller/MainController.scala
@@ -9,42 +9,43 @@ import play.api.mvc.{Action, Controller}
 import src.main.scala.com.supersaiyyans.actors.{MQTTPublisherProxySupervisor, MQTTSubscriberProxySupervisor}
 import src.main.scala.com.supersaiyyans.store.ServiceStore
 
-object MainController extends Controller{
+object MainController extends Controller {
 
-  val mqttPublisherProxySupervisor = Akka.system.actorOf(Props[MQTTPublisherProxySupervisor],"MQTTPublisherProxySupervisor")
- val mqttSubscriberProxySupervisor = Akka.system.actorOf(Props[MQTTSubscriberProxySupervisor],"MQTTSubscriberProxySupervisor")
+  val mqttPublisherProxySupervisor = Akka.system.actorOf(Props[MQTTPublisherProxySupervisor], "MQTTPublisherProxySupervisor")
+  val mqttSubscriberProxySupervisor = Akka.system.actorOf(Props[MQTTSubscriberProxySupervisor], "MQTTSubscriberProxySupervisor")
 
 
-  def serviceCommand= Action(parse.json){
-    request=>
+  def serviceCommand = Action(parse.json) {
+    request =>
       request.body.validate[JsonCmdPacket].map {
-        packet=>
+        packet =>
           mqttPublisherProxySupervisor ! packet
-          Ok(packet.toString).withHeaders("Access-Control-Allow-Origin"->"*")
-      }.recoverTotal(e=>BadRequest)
+          Ok(packet.toString).withHeaders("Access-Control-Allow-Origin" -> "*")
+      }.recoverTotal(e => BadRequest)
 
 
   }
 
-  def option(path: String) = Action{
-      Ok("").withHeaders(
-        "Access-Control-Allow-Origin" -> "*",
-        "Access-Control-Allow-Methods" -> "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers" -> "Accept, Origin, Content-type, X-Json, X-Prototype-Version, X-Requested-With",
-        "Access-Control-Allow-Credentials" -> "true",
-        "Access-Control-Max-Age" -> (0).toString)
+  def option(path: String) = Action {
+    Ok("").withHeaders(
+      "Access-Control-Allow-Origin" -> "*",
+      "Access-Control-Allow-Methods" -> "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers" -> "Accept, Origin, Content-type, X-Json, X-Prototype-Version, X-Requested-With",
+      "Access-Control-Allow-Credentials" -> "true",
+      "Access-Control-Max-Age" -> (0).toString)
   }
-  def listServices = Action{
-    request=>
+
+  def listServices = Action {
+    request =>
       Ok(
         Json.toJson(
-        ServiceStore
-          .listAll()
-          .map{
-            deviceWithService=>
-              deviceWithService._2.toJson()
-          }.toArray))
+          ServiceStore
+            .listAll()
+            .map {
+              deviceWithService =>
+                deviceWithService._2.toJson()
+            }.toArray))
         .withHeaders("Content-Type" -> "application/json"
-        ,"Access-Control-Allow-Origin"->"*")
+          , "Access-Control-Allow-Origin" -> "*")
   }
 }
