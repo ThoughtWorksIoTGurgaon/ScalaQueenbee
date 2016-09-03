fdcc3d111162b192dd8f32134740be62e8acc944
Dhruv Kapur <dhruvk@thoughtworks.com>
2016-09-03 16:18:59 +0530

    Send unexpected message to the sender if the message is not understood

--- app/src/main/scala/com/supersaiyyans/actors/ServicesRepoActor.scala
+++ app/src/main/scala/com/supersaiyyans/actors/ServicesRepoActor.scala
@@ -1,33 +1,37 @@
 package com.supersaiyyans.actors
 
-import akka.actor.{Props, FSM}
+import akka.actor.{ActorLogging, FSM, Props}
 import ServicesRepoActor._
 
-class ServicesRepoActor extends FSM[State,Data]{
+class ServicesRepoActor extends FSM[State, Data] with ActorLogging {
 
-  startWith(Running,ServicesData(Map.empty))
+  startWith(Running, ServicesData(Map.empty))
 
   // TODO - add exception for unexpected messages
 
-  when(Running){
-    case Event(command: UpdateServiceData,oldData: ServicesData) =>
+  when(Running) {
+    case Event(command: UpdateServiceData, oldData: ServicesData) =>
       val newData =
         oldData
           .data
-          .updated(command.serviceData.serviceId,command.serviceData)
+          .updated(command.serviceData.serviceId, command.serviceData)
       stay using ServicesData(newData)
 
-    case Event(FetchAll,currentState: ServicesData) =>
+    case Event(FetchAll, currentState: ServicesData) =>
       sender ! currentState.data.values.toList
       stay
 
-    case Event(fetchData: FetchServiceData,currentState: ServicesData) =>
+    case Event(fetchData: FetchServiceData, currentState: ServicesData) =>
       sender ! currentState.data.get(fetchData.serviceId)
       stay
 
-    case Event(newService : AddService, oldData : ServicesData) =>
+    case Event(newService: AddService, oldData: ServicesData) =>
       val newData = oldData.data.updated(newService.serviceData.serviceId, newService.serviceData)
       stay using ServicesData(newData)
+
+    case _ => log.error("Unexpected message found")
+      sender ! UnexpectedMessage
+      stay
   }
 
 }
@@ -37,22 +41,31 @@ object ServicesRepoActor {
   def props = Props[ServicesRepoActor]
 
   sealed trait State
+
   object Initializing extends State
+
   object Running extends State
 
   sealed trait Data
-  sealed case class ServicesData(data: Map[String,ServiceData]) extends Data
+
+  sealed case class ServicesData(data: Map[String, ServiceData]) extends Data
 
   trait ServiceState
-  case class SwitchServiceState(value: String) extends ServiceState // TODO : Enchantress and this should probably use the same thing
-  case class ServiceData(name: String,serviceId: String,deviceId: String,state: ServiceState)
+
+  case class SwitchServiceState(value: String) extends ServiceState
+
+  // TODO : Enchantress and this should probably use the same thing
+  case class ServiceData(name: String, serviceId: String, deviceId: String, state: ServiceState)
 
   sealed trait SupportedEvent
-  object FetchAll extends SupportedEvent
-  case class AddService(serviceData: ServiceData) extends SupportedEvent
-  case class UpdateServiceData(serviceData: ServiceData) extends SupportedEvent
-  case class FetchServiceData(serviceId: String) extends SupportedEvent
 
+  object FetchAll extends SupportedEvent
+
+  case class AddService(serviceData: ServiceData) extends SupportedEvent
+
+  case class UpdateServiceData(serviceData: ServiceData) extends SupportedEvent
+
+  case class FetchServiceData(serviceId: String) extends SupportedEvent
 
 
 }
--- app/src/test/scala/com/supersaiyyans/actors/ServicesRepoActorTest.scala
+++ app/src/test/scala/com/supersaiyyans/actors/ServicesRepoActorTest.scala
@@ -1,9 +1,25 @@
 package com.supersaiyyans.actors
 
-import akka.actor.ActorSystem
-import akka.testkit.{TestFSMRef, TestKit}
+import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.testkit.{TestActorRef, TestFSMRef, TestKit}
 import com.supersaiyyans.actors.ServicesRepoActor._
 import org.scalatest.{FunSpec, Matchers}
+
+case object UnexpectedMessage  // Todo : Make it more specific
+case object SomeUnknownMessage
+
+class DummyActor(actorRef : ActorRef) extends Actor {
+
+  def sendUnexpectedMessage = actorRef ! SomeUnknownMessage
+
+  var recievedUnxpectedMessage = false
+
+  override def receive: Receive = {
+    case UnexpectedMessage => recievedUnxpectedMessage = true
+  }
+}
+
 
 class ServicesRepoActorTest extends FunSpec with Matchers {
 
@@ -45,6 +61,21 @@ class ServicesRepoActorTest extends FunSpec with Matchers {
       serviceRepoActorRef.stateName should be(Running)
       serviceRepoActorRef.stateData should be(ServicesData(Map("SW101" -> switchServiceData)))
     }
+
+    it("Should notify sender about unexpected message if the message is not received/present") {
+      implicit val actorSystem = ActorSystem()
+
+      val serviceRepoActorRef = TestFSMRef(new ServicesRepoActor)
+
+      val ref: TestActorRef[DummyActor] = TestActorRef(Props(new DummyActor(serviceRepoActorRef)))
+      val actor: DummyActor = ref.underlyingActor
+
+      actor.sendUnexpectedMessage
+
+      serviceRepoActorRef.stateName should be(Running)
+      actor.recievedUnxpectedMessage should be(true)
+    }
+
 
   }
 }
