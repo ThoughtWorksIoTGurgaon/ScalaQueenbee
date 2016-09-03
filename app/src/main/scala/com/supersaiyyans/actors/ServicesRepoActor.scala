aa2d1d869cea95f95e531c9760ce2dd7bdffe6a5
Dhruv Kapur <dhruvk@thoughtworks.com>
2016-09-03 13:35:21 +0530

    Add todo for using same service state in repo actor and enchantress

--- app/src/main/scala/com/supersaiyyans/actors/ServicesRepoActor.scala
+++ app/src/main/scala/com/supersaiyyans/actors/ServicesRepoActor.scala
@@ -38,7 +38,7 @@ object ServicesRepoActor {
   sealed case class ServicesData(data: Map[String,ServiceData]) extends Data
 
   trait ServiceState
-  case class SwitchServiceState(value: String) extends ServiceState
+  case class SwitchServiceState(value: String) extends ServiceState // TODO : Enchantress and this should probably use the same thing
   case class ServiceData(name: String,serviceId: String,deviceId: String,state: ServiceState)
 
   sealed trait SupportedEvent
