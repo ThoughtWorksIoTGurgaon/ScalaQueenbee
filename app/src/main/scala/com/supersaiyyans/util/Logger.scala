a6cce1a965e111e47d5b83401f29403f99ef193b
Dhruv Kapur <dhruvk@thoughtworks.com>
2017-02-09 19:41:35 +0530

    Use play logger for logging, not sure if I like it though.

--- app/src/main/scala/com/supersaiyyans/util/Logger.scala
+++ app/src/main/scala/com/supersaiyyans/util/Logger.scala
@@ -3,10 +3,10 @@ package com.supersaiyyans.util
 object Logger {
   def debugWithArgs(message: String, args: (String,String)*) = {
     debug(message)
-    args.foreach(x=>println(s"${x._1}: ${x._2}"))
+    args.foreach(x=>debug(s"${x._1}: ${x._2}"))
   }
   def debug(message: String) = {
-    println(s"\n-------------${message}------------")
+    play.api.Logger.debug(s"\n-------------$message------------")
   }
 
 }
