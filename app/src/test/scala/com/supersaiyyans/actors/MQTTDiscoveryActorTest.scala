3417ac0525f9d12a3e61a4d71386984bd17e62ce
Dhruv Kapur <dhruvk@thoughtworks.com>
2017-02-11 16:57:02 +0530

    Add test for checking MqttManager recieves a connect message

---
+++ app/src/test/scala/com/supersaiyyans/actors/MQTTDiscoveryActorTest.scala
@@ -0,0 +1,29 @@
+package src.test.scala.com.supersaiyyans.actors
+
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestFSMRef, TestKit, TestProbe}
+import com.supersaiyyans.actors.MQTTDiscoveryActor
+import net.sigusr.mqtt.api.Connect
+import org.scalatest.{FunSpecLike, FunSuite, FunSuiteLike, ShouldMatchers}
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+
+class MQTTDiscoveryActorTest() extends TestKit(ActorSystem("MQTTDiscoveryActor")) with FunSpecLike with ShouldMatchers with ImplicitSender {
+
+  val dummyMqttManager = TestProbe()
+
+  describe("MQTT Discovery actor") {
+    it("Should try to send connection messages to MQTT manager immediately and then every configured time") {
+      val mqttDiscoveryActor = system.actorOf(Props(new MQTTDiscoveryActor {
+        override val mqttManager = dummyMqttManager.ref
+      }))
+
+      dummyMqttManager.expectMsg(1 seconds, Connect("QUEENBEE_MQTT_DISCOVERY_ACTOR_CONNECTING"))
+    }
+  }
+
+
+
+}
